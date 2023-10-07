023d9f313709f829d6817c45cd6994cbc7db0bdf
Sanjaymark16 <sanjaymark16@gmail.com>
2023-10-07 20:24:38 +0530

    added projects

--- src/Components/Contact Info.js
+++ src/Components/Contact Info.js
@@ -7,9 +7,9 @@ export const ContactInfo = () =>
             <div className="text-6xl heading p-2 my-10 hover-effect1">Connect with Me</div>
             <div>
                 <a href="https://github.com/Sanjaymark" target="_blank" rel="noopener noreferrer" className="btn btn-link btn-lg">GitHub</a>
-                <a href="https://www.linkedin.com/in/sanjay-murgesan-973699257/" target="_blank" rel="noopener noreferrer" className="btn btn-link btn-lg">LinkedIn</a>
+                {/* <a href="" target="_blank" rel="noopener noreferrer" className="btn btn-link btn-lg">Resume</a> */}
             </div>
-            <div className="text-lg italic mailid">
+            <div className="text-lg italic mailid pb-6">
                 sanjaymark16@gmail.com
             </div>
             
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,109 +3,151 @@ import React from "react";
 export const Projects = () =>
 {
     return(
-        <div className="mt-20 h-full">
+        <div className="mt-20 h-full light">
             <div className="text-6xl heading p-2 my-16 hover-effect1">Projects</div>
-            <div className="overflow-x-auto">
-              <table className="table">
-              {/* head */}
-              <thead>
-              </thead>
-              <tbody>
-              {/* row 1 */}
-                <tr>
-                  <th>
-                    <label>
-                      <input type="checkbox" className="checkbox" />
-                    </label>
-                  </th>
-                  <td>
-                    <div className="flex items-center space-x-3">
-                      <div className="avatar">
-                        <div className="mask mask-squircle w-12 h-12">
-                          <img src="https://www.vetbossel.in/wp-content/uploads/2020/09/gst-billing-app-android-studio.png" alt="Avatar Tailwind CSS Component" />
-                        </div>
+            <div className="flex flex-col w-full">
+              <div className="grid h-full ">
+                <div className="flex flex-col w-full">
+  		            <div className="grid h-full ">
+			              <span className="text-4xl italic mb-12 projects">Inventory Billing Web App</span>
+                    <div className="flex">
+                      <div className="w-1/5">
+                        <img className="img-s" src="https://androidcure.com/wp-content/uploads/2020/06/choose-right-Inventory-Management-Software.jpg" alt="Inventory Billing" width="350" height="200"></img>
                       </div>
-                      <div>
-                        <div className="font-bold">Inventory Billing App</div>
+                      <div className="w-1/5 text-xl italic">
+                        <div className="text-4xl m-4 heading">Key Features</div>
+                        <ol className="">
+                          <li>User Authentication</li>
+                          <li>Product Catalog</li>
+                          <li>Admin Privileges</li>
+                          <li>CRUD Operations</li>
+                          <li>Shopping Cart</li>
+                          <li>Order Processing</li>
+                          <li>Individual Orders</li>
+                        </ol>
                       </div>
-                    </div>
-                  </td>
-                  <td>
-                    <a
-                      href="https://endearing-horse-15c68d.netlify.app"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-blue-500 hover:underline"
-                    >
-                    Billing App
-                    </a>
-                  </td>
-                </tr>
-              {/* row 2 */}
-              <tr>
-                <th>
-                  <label>
-                    <input type="checkbox" className="checkbox" />
-                  </label>
-                </th>
-                <td>
-                  <div className="flex items-center space-x-3">
-                    <div className="avatar">
-                      <div className="mask mask-squircle w-12 h-12">
-                        <img src="https://global-uploads.webflow.com/5ebea55066f36f531dec5b32/628553b6a7a763b0ed126a9c_I41SrzFSarnJwGgxSXv0dIWn4AwVa80Ly_B8-afRWUE3eyezBFQggd8Yq5dLWyqQjThPM2N5Ajwc6KlEOwKo01GI887rWDyahPHiIbMCi6bwkRjqd1NLs8VFnWXC1RLAQ7xXPZ1mo2VBzHCwCw.png" alt="Avatar Tailwind CSS Component" />
+                      <div className="w-2/5 text-xl italic">
+                        <div className="text-4xl m-4 heading">Description</div>
+                        <p>
+                          Had conceptualized, designed, and developed a comprehensive Inventory Billing Web Application that provides a 
+                          seamless and efficient solution for product management, user authentication, and order processing. This web application
+                          not only highlights my expertise in MERN stack development but also underscores my capability to craft a user-friendly
+                          and interactive platform. It serves as a valuable tool for businesses aiming to optimize their inventory management 
+                          processes and elevate the overall shopping experience for their clientele.
+                        </p>
+                      </div>
+                      <div className="w-1/5  text-xl link">
+                          <a href="https://ubiquitous-licorice-0a3500.netlify.app/" target="_blank">View Web App</a>
                       </div>
                     </div>
-                    <div>
-                      <div className="font-bold">Student-Teacher-Formik</div>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  <a
-                    href="https://frolicking-gumdrop-ca2ead.netlify.app/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-500 hover:underline"
-                  >
-                    Formik App
-                  </a>
-                </td>
-              </tr>
-              {/* row 3 */}
-              <tr>
-                <th>
-                  <label>
-                    <input type="checkbox" className="checkbox" />
-                  </label>
-                </th>
-                <td>
-                  <div className="flex items-center space-x-3">
-                    <div className="avatar">
-                      <div className="mask mask-squircle w-12 h-12">
-                        <img src="https://www.wodumedia.com/wp-content/uploads/sites/2/sites/1000016/2018/08/Dashboard-dashboard.png" alt="Avatar Tailwind CSS Component" />
+		              </div> 
+  		            <div className="divider"></div> 
+  		            <div className="grid h-full   ">
+			              <span className="text-4xl italic mb-12 projects">Movie Ticket Booking Web App</span>
+                    <div className="flex">
+                      <div className="w-1/5 h-full">
+                      <img className="img-s" src="https://hirharang.com/wp-content/uploads/2018/07/Movie-Ticket-Booking-Online.jpg" alt="Movie Booking"></img>
+
+                      </div>
+                      <div className="w-1/5 text-xl italic">
+                        <div className="text-4xl m-4 heading">Key Features</div>
+                        <ul> 
+                          <li>User Authentication</li>
+                          <li>Movie Listings</li>
+                          <li>Admin Privileges</li>
+                          <li>Showtime Management</li>
+                          <li>Ticket Booking</li>
+                          <li>Booking History</li>
+                          <li>Booking Cancellation</li>
+                        </ul>
+                      </div>
+                      <div className="w-2/5 text-xl italic">
+                        <div className="text-4xl m-4 heading">Description</div>
+                        <p>
+                        Developed an intuitive web application designed for the seamless discovery, reservation, and 
+                        management of movie tickets. This project, meticulously crafted using contemporary web technologies, serves as a 
+                        testament to my expertise in full-stack web development, encompassing aspects such as user authentication and
+                         role-based access control. It stands as a prominent example of my unwavering dedication to constructing interactive 
+                         and highly functional applications that significantly elevate user experiences within the digital realm.
+                        </p>
+                      </div>
+                      <div className="w-1/5  text-xl link">
+                          <a href="https://steady-cocada-c69147.netlify.app/" target="_blank">View Web App</a>
                       </div>
                     </div>
-                    <div>
-                      <div className="font-bold">A Simple Dashboard</div>
+		              </div>
+	              </div>
+              </div> 
+              <div className="divider"></div> 
+              <div className="grid h-full  ">
+                <div className="flex flex-col w-full">
+  		            <div className="grid h-full card   ">
+			              <span className="text-4xl italic mb-12 projects">Student Details Web App</span>
+                    <div className="flex">
+                      <div className="w-1/5">
+                        <img className="img-s" src="https://stack.convex.dev/_next/image?url=https:%2F%2Fcdn.sanity.io%2Fimages%2Fts10onj4%2Fproduction%2Ff911b37cad0a64866ccf44790dcf0fe60fc6bf7b-1200x628.png&w=828&q=75" alt="Student Details" width="300" height="200"></img>
+                      </div>
+                      <div className="w-1/5 text-xl italic">
+                        <div className="text-4xl m-4 heading">Key Features</div>
+                        <ul> 
+                          <li>User Authentication</li>
+                          <li>User Account Requirement</li>
+                          <li>Admin Privileges</li>
+                          <li>Account Management</li>
+                          <li>Student Information Access</li>
+                          <li>CRUD Operations</li>
+                        </ul>
+                      </div>
+                      <div className="w-2/5 text-xl italic">
+                        <div className="text-4xl m-4 heading">Description</div>
+                        <p>
+                          Designed a Student Details App to efficiently manage student information. This application incorporates essential 
+                          features such as user authentication and role-based access control, providing a comprehensive solution for accessing
+                          and maintaining student records. It is poised to serve as an invaluable tool for educational institutions and 
+                          organizations seeking to streamline student record management while upholding data privacy and integrity.
+                        </p>
+                      </div>
+                      <div className="w-1/5  text-xl link">
+                          <a href="https://lambent-crostata-04f984.netlify.app/" target="_blank">View Web App</a>
+                      </div>
                     </div>
-                  </div>
-                </td>
-                <td>
-                  <a
-                    href="https://tangerine-jelly-2bc108.netlify.app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-500 hover:underline"
-                  >
-                    Dashboard App
-                  </a>
-                </td>
-              </tr>
-      
-            </tbody>
-    
-          </table>
-        </div>
-      </div>
+		              </div> 
+  		            <div className="divider"></div> 
+  		            <div className="grid h-full  ">
+			              <span className="text-4xl italic mb-12 projects">Blogs Web App</span>
+                    <div className="flex">
+                      <div className="w-1/5 ">
+                        <img className="img-s" src="https://thumbs.dreamstime.com/b/blog-digital-background-internet-concept-34568237.jpg" alt="Blog" width="300" height="200"></img>
+                      </div>
+                      <div className="w-1/5  text-xl italic">
+                        <div className="text-4xl m-4 heading">Key Features</div>
+                        <ul>
+                          <li>User Authentication</li>
+                          <li>User Account Requirement</li>
+                          <li>CRUD Operations on Blogs</li>
+                          <li>Admin Privileges</li>
+                          <li>User-Specific CRUD</li>
+                          <li>Individual Blog Viewing</li>
+                        </ul>
+                      </div>
+                      <div className="w-2/5 text-xl italic">
+                        <div className="text-4xl m-4 heading">Description</div>
+                        <p>
+                          Created a feature-rich Blogs App, a web-based platform tailored for bloggers and readers. It emphasizes 
+                          user authentication, data security, and personalized experiences, catering to both groups. This project 
+                          showcases my expertise in web application development, particularly in user authentication and data security.
+                           It offers bloggers a platform to share their thoughts and enables readers to explore engaging content while 
+                           maintaining a secure and personalized experience.
+                        </p>
+                      </div>
+                      <div className="w-1/5 text-xl link">
+                          <a href="https://cheerful-rolypoly-6bcafe.netlify.app/" target="_blank">View Web App</a>
+                      </div>
+                    </div>
+		              </div>
+	              </div>
+              </div>
+            </div>
+          </div>
     )
 }
