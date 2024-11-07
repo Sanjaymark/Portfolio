32504d5c4eda7402b1de644eec1ab79355841d41
Sanjaymark16 <sanjaymark16@gmail.com>
2024-11-07 10:15:41 +0530

    Resume Removed

--- src/Components/Contact Info.js
+++ src/Components/Contact Info.js
@@ -7,7 +7,6 @@ export const ContactInfo = () =>
             <div className="text-6xl heading p-2 my-10 hover-effect1">Connect with Me</div>
             <div>
                 <a href="https://github.com/Sanjaymark" target="_blank" rel="noopener noreferrer" className="btn btn-link btn-lg">GitHub</a>
-                <a href="https://drive.google.com/file/d/1ezgJJcnXtBzVlPkSPznOPV2urz8ZG56A/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="btn btn-link btn-lg">Resume</a>
             </div>
             <div className="text-lg italic mailid pb-6">
                 sanjaymark16@gmail.com
