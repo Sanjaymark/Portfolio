a815e4d82f896a7aa0c99b13f1052aa0b2fde8b9
Sanjaymark16 <sanjaymark16@gmail.com>
2024-11-07 10:11:06 +0530

    Updated Contact Info.js by Resume

--- src/Components/Contact Info.js
+++ src/Components/Contact Info.js
@@ -7,7 +7,7 @@ export const ContactInfo = () =>
             <div className="text-6xl heading p-2 my-10 hover-effect1">Connect with Me</div>
             <div>
                 <a href="https://github.com/Sanjaymark" target="_blank" rel="noopener noreferrer" className="btn btn-link btn-lg">GitHub</a>
-                <a href="https://drive.google.com/file/d/1vt0qvM0Yq5_j1jqHOrQApFqvIb2GxZdz/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="btn btn-link btn-lg">Resume</a>
+                <a href="https://drive.google.com/file/d/1ezgJJcnXtBzVlPkSPznOPV2urz8ZG56A/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="btn btn-link btn-lg">Resume</a>
             </div>
             <div className="text-lg italic mailid pb-6">
                 sanjaymark16@gmail.com
